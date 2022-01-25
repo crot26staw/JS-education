0ad8338603713d403eccda5f0669fdae3afc6c67
Dima <crot26staww@mail.ru>
2022-01-25 15:30:43 +0300

    конструкция свитч

--- js/script.js
+++ js/script.js
@@ -6,14 +6,31 @@ if (4 == 9) {
     console.log('Error');
 }
 
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error'); //Тернарный оператор
+
 const num = 50;
 
-if (num < 49) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Много');
-} else {
-    console.log('Ok');
+switch (num) {     //конструкция свич всегда проверяет на строгое сравнение
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:  
+        console.log('Неверно');
+        break;
+    case 50:  
+        console.log('В точку');
+        break; 
+    default:
+        console.log('Не в этот раз');
+        break;      
 }
-
-(num === 50) ? console.log('Ok') : console.log('Error'); //Тернарный оператор
