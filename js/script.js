9ec633f6520520d5e80516142d9b8390f79d829b
Dima <crot26staww@mail.ru>
2022-01-25 15:28:17 +0300

    Условные операторы if else

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
 
+const num = 50;
 
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok');
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    generes: [],  //Массив
-    private: false
-};
-
-const a = prompt('Один из последних фильмов', ''),
-      b = prompt('На сколько оцените его?', ''), 
-      c = prompt('Один из последних фильмов', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;   
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+(num === 50) ? console.log('Ok') : console.log('Error'); //Тернарный оператор
