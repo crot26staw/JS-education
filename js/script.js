0d0c7c8a0f77c4b82393b77d9c322007a4f5bcf9
Dima <crot26staww@mail.ru>
2022-01-26 12:20:49 +0300

    Практика условия и циклы for, while, do

--- js/script.js
+++ js/script.js
@@ -1,23 +1,76 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: [],  //Массив
+    private: false
+};
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних фильмов', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+//     personalMovieDB.movies[a] = b;      
 // }
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
+// ПЕРВЫЙ МОЙ ВАРИАНТ
 
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        //break
-        continue;
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt('Один из последних фильмов', ''),
+//           b = prompt('На сколько оцените его?', '');
+//     i++;
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     } 
+       
+// }
+
+// ВТОРОЙ МОЙ ВАРИАНТ
+
+let i = 0;
+do {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    i++;
+      
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+ 
+        personalMovieDB.movies[a] = b;
+        console.log('done'); 
+    } else {
+    console.log('error');
+    i--;
     }
-
-    console.log(i);
 }
+while (i < 2);
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы класический киноман');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('ПРоизошла ошибка');
+}
+
+console.log(personalMovieDB);
+
