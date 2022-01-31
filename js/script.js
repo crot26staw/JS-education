c71a741747ea74c7693f1027e408c5c3357258a6
Dima <crot26staww@mail.ru>
2022-01-31 19:57:34 +0300

    Передача по ссылке или значению, копирование объектов

--- js/script.js
+++ js/script.js
@@ -1,26 +1,80 @@
 "use strict";
 
-const arr = [1, 2, 3, 6, 8];
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
 
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// });
+// const copy = obj; // Передаёт не саму структуру а ссылку на объект
 
-// // console.log(arr.length); // Кол-во элементов массива
+// copy.a = 10;
 
-// //arr.pop(); //удаляет последний элемент массива
-// arr.push(10); //добавляет элемент в конец массива
+// console.log(copy);
 
-// console.log(arr);
+function copy(mainObj) {
+    let objCopy = {};
+    
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+    return objCopy;
+}
 
-// for (let value of arr) {       //еще один вариант
-//     console.log(value);
-// }
+const numbers = {
+    a: 2,
+    b: 3,
+    c: {
+        x: 5,
+        y: 7
+    }
+};
 
-const str = prompt("", "");
-const products = str.split(", ");
-console.log(products.join('; '));
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+
+console.log(newNumbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+// console.log(Object.assign(numbers, add));  // соединяет объекты
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();   //копируем массив
+
+newArray[1] = 'sfdghjryhj';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],        //еще 1 способ
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);      
+
+
+function log(a, b, c) {  //еще 1 способ
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+const newAAray = [...array];  //Так тоже копирует
+
+
+const q = {
+    one: 1,           //И так тоже можно
+    two: 2
+};
+
+const newQ = {...q};
